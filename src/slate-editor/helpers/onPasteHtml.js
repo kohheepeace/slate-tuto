1f99455f5e4ba32eefd8f542035e2e475146690f
kohheepeace <kohheepeace@kohheepeacenoMacBook.local>
2018-05-11 02:45:25 +0900

    Finish

---
+++ src/slate-editor/helpers/onPasteHtml.js
@@ -0,0 +1,19 @@
+import Html from 'slate-html-serializer';
+import { getEventTransfer } from 'slate-react';
+import rules from '../rules/index';
+
+const serializer = new Html({ rules });
+
+const onPasteHtml = (e, change) => {
+  if (e.shiftKey) return;
+  const transfer = getEventTransfer(e);
+  const { html, rich, text } = transfer;
+  if (rich) {
+    return change.insertText(text);
+  }
+  const { document } = serializer.deserialize(html);
+  change.insertFragment(document);
+  return true;
+};
+
+export default onPasteHtml;
