36877a1498d49084fccb4948f708ec8eb2b22427
kohheepeace <kohheepeace@gmail.com>
2018-09-16 08:26:59 +0900

    Delete elsint

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: [
-          { loader: 'babel-loader' },
-          { loader: 'eslint-loader',
-            options: {
-              fix: true
-            }
-          }
+          { loader: 'babel-loader' }
         ]
       },
       {
